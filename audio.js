577e9811acd3994cbb8ae7f4fec32cc9a6e994be
AlisamfP <eatliightning@gmail.com>
2014-08-05 20:31:41 -0700

    fixed getElementsById to getElementById

--- audio.js
+++ audio.js
@@ -1,6 +1,5 @@
-var audioTag = document.getElementsById('pronounce');
-
 $(document).ready(function(){
+var audioTag = document.getElementById('pronounce');
 	$('#sayName').on('click', function(){
 		audioTag.play();
 	});
