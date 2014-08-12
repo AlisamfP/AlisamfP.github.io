1730212f38af0553785c24f04eac7277a5d4531e
AlisamfP <eatliightning@gmail.com>
2014-08-11 18:55:19 -0700

    favicon is working

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-// var about = require('./routes/about');
-// var contact = require('./routes/contact');
-// var blog = require('./routes/blog');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -24,9 +21,6 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-// app.use('/about', about);
-// app.use('/contact', contact);
-// app.use('/blog', blog);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
