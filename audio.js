95b8bbfa05ee760c220a3a9fe0c0c16fc6f7c688
Alisa Palson <eatliightning@gmail.com>
2024-06-18 14:45:38 -0700

    started working with react app

--- audio.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-var audioTag = document.getElementById('pronounce');
-	$('#sayName').on('click', function(){
-		audioTag.play();
-	});
-});
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header>
+        <h1 className='title'>Alisa Palson</h1>
+        <nav className='mainNav'>
+          <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    );
+}
+
+export default Header;
