f741bc6afc9fe966a6736b0d84ab8009d5eb575f
Alisa Palson <eatliightning@gmail.com>
2025-09-30 18:09:37 -0700

    fix list item accessibility issue

--- src/Components/ListItem.tsx
+++ src/Components/ListItem.tsx
@@ -5,10 +5,10 @@ type ListItemProps = {
 const ListItem = ({ text }: ListItemProps) => {
     // #8226 is just a smaller bullet point
     return (
-        <div className="grid grid-cols-[max-content_1fr] gap-2">
+        <li className="grid grid-cols-[max-content_1fr] gap-2">
             <span>&#8226;</span>
-            <li>{" "}{text}</li>
-        </div>
+            <div>{" "}{text}</div>
+        </li>
     )
 }
 
